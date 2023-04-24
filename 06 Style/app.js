da2abc54c74a9dd2aec38efc6056cf5e36e03fcf
Viridiana Leal <LERV060816MHGLMRA8@soycecytem.mx>
2023-04-24 09:25:07 -0600

    style 2

--- 06 Style/app.js
+++ 06 Style/app.js
@@ -3,17 +3,20 @@ const cambiar=()=>{
     let colorFondo=document.querySelector("#color").value;
     let colorTexto=document.querySelector("#colort").value;
     let colorBorde=document.querySelector("#colorb").value;
-    let texto=document.querySelector("#texto").value
     let figura=document.querySelector("#figura").value;
+    let br=(figura=="1")?"0%":"100%" 
+    let texto=document.querySelector("#texto").value
+    
     let alto=document.querySelector("#alto").value;
     let ancho=document.querySelector("#ancho").value;
-    let br=(figura=="1")?"0%":"100%" 
+    document.querySelector("#vancho").innerHTML=ancho
+    document.querySelector("#valto").innerHTML=alto 
+    div.style.width=ancho+"px"
+    div.style.height=alto+"px"
     div.style.borderRadius=br
     div.style.background=colorFondo
     div.style.color=colorTexto
     div.style.borderColor=colorBorde
     div.innerHTML=texto
-    div.style.width=ancho+"px"
-    div.style.height=alto+"px"
 
 }
